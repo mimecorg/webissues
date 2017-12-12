006ac45181b06f686d3a128e688ce58c01bb1afd
mimecorg <mimec@mimec.org>
2017-12-12 16:46:23 +0000

    remove unnecessary elements from the page layout
    git-svn-id: svn+ssh://svn.code.sf.net/p/webissues/code/trunk/server@2378 c8fa975d-6fd4-4e76-b3b8-1a99b2ee3549

--- common/pagelayout.html.php
+++ common/pagelayout.html.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title><?php echo $pageTitle ?> | <?php echo $siteName ?></title>
-  <link rel="shortcut icon" href="<?php echo $this->url( $icon ) ?>" type="image/vnd.microsoft.icon"> 
+  <link rel="shortcut icon" href="<?php echo $this->url( $icon ) ?>" type="image/vnd.microsoft.icon">
 <?php foreach ( $cssFiles as $file ): ?>
   <link rel="stylesheet" href="<?php echo $this->url( $file ) ?>" type="text/css">
 <?php endforeach ?>
@@ -18,52 +18,21 @@
   <script type="text/javascript">
 <?php echo $inlineCode ?>
   </script>
-<?php endif ?>  
+<?php endif ?>
 </head>
 <body>
 
 <div id="header">
-  <img id="site-logo" src="<?php echo $this->url( '/common/images/webissues-logo.png' )?>" alt="WebIssues">
-  <div id="site-name"><?php echo $siteName ?></div>
+  <a href="<?php echo $this->url( $homeUrl ) ?>"><img id="site-logo" src="<?php echo $this->url( '/common/images/webissues-logo.png' ) ?>" alt="WebIssues"></a>
+  <div id="site-name"><?php echo $this->link( $homeUrl, $siteName ) ?></div>
   <div id="header-right">
 <?php
-    if ( $isAuthenticated || $isAnonymous ):
-        if ( $isAdministrator ):
-            echo $this->link( '/admin/index.php', $this->tr( 'Administration Panel' ) ) . ' | ';
-        endif;
-        echo $this->link( '/client/index.php', $this->tr( 'Web Client' ) ) . ' | ';
-    endif;
-    echo $this->link( $mobileVersionUrl, $this->tr( 'Mobile Version' ), array( 'class' => 'switch-client' ) ) . ' | ';
     echo $this->tr( 'WebIssues %1', null, WI_VERSION ) . ' | ';
     echo $this->link( $manualUrl, $this->tr( 'Manual' ), array( 'target' => '_blank' ) );
 ?>
   </div>
-  <div id="infobar-left">
-<?php
-    foreach ( $breadcrumbs as $breadcrumb ):
-        echo $this->link( $breadcrumb[ 'url' ], $breadcrumb[ 'name' ] ) . ' &raquo; ';
-    endforeach;
-    echo $pageTitle;
-?>
-  </div>
-  <div id="infobar-right">
-<?php
-    if ( $isAuthenticated ):
-        echo $this->tr( 'Logged in as: %1', null, $userName ) . ' | ';
-    endif;
-    if ( $isAuthenticated || $canLogIn ):
-        echo $this->link( '/client/tools/index.php', $this->tr( 'Tools' ) ) . ' | ';
-    endif;
-    if ( $isAuthenticated ):
-        echo $this->link( '/index.php', $this->tr( 'Log Out' ) );
-    elseif ( $canLogIn ):
-        echo $this->link( $loginPageUrl, $this->tr( 'Log In' ) );
-        if ( $canRegister ):
-            echo ' | ' . $this->link( '/register.php', $this->tr( 'Register' ) );
-        endif;
-    endif;
-?>
-  </div>
+  <div id="infobar-left"><?php echo $pageTitle ?></div>
+  <div id="infobar-right"></div>
 </div>
 
 <div id="body">
--- common/pagelayout.inc.php
+++ common/pagelayout.inc.php
@@ -29,16 +29,6 @@ class Common_PageLayout extends System_Web_Component
 
     protected function execute()
     {
-        $principal = System_Api_Principal::getCurrent();
-
-        $this->isAuthenticated = $principal->isAuthenticated();
-        $this->isAdministrator = $principal->isAdministrator();
-        $this->userName = $principal->getUserName();
-
-        $this->isAnonymous = false;
-        $this->canLogIn = false;
-        $this->canRegister = false;
-
         $application = System_Core_Application::getInstance();
 
         $this->siteName = $this->tr( 'WebIssues' );
@@ -47,55 +37,18 @@ class Common_PageLayout extends System_Web_Component
                 $serverManager = new System_Api_ServerManager();
                 $server = $serverManager->getServer();
                 $this->siteName = $server[ 'server_name' ];
-
-                if ( !$principal->isAuthenticated() ) {
-                    $this->isAnonymous = $serverManager->getSetting( 'anonymous_access' ) == 1;
-                    if ( $this->isAnonymous ) {
-                        $this->canLogIn = $this->request->isRelativePathUnder( '/client' );
-                        if ( $this->canLogIn ) {
-                            $this->loginPageUrl = $application->getLoginPageUrl();
-                            $this->canRegister = $serverManager->getSetting( 'self_register' ) == 1 && $serverManager->getSetting( 'email_engine' ) != null;
-                        }
-                    }
-                }
             }
         } catch ( Exception $ex ) {
             $application->handleException( $ex );
         }
 
-        if ( $this->request->isRelativePath( '/client/index.php' ) )
-            $this->mobileVersionUrl = new System_Web_RawValue( $this->filterQueryString( '/mobile/client/index.php', array( 'project', 'folder', 'type', 'issue' ) ) );
-        else if ( $this->isAuthenticated || $this->canLogIn )
-            $this->mobileVersionUrl = '/mobile/client/index.php';
-        else
-            $this->mobileVersionUrl = '/mobile/index.php';
-
         $this->pageTitle = $this->view->getSlot( 'page_title', $this->tr( 'Untitled page' ) );
 
-        if ( $this->request->isRelativePathUnder( '/client' ) ) {
-            $homeUrl = '/client/index.php';
-            $homeName = $this->tr( 'Web Client' );
-        } else if ( $this->request->isRelativePathUnder( '/admin' ) && $principal->isAdministrator() ) {
-            $homeUrl = '/admin/index.php';
-            $homeName = $this->tr( 'Administration Panel' );
-        }
-
-        $parents = $this->view->getSlot( 'breadcrumbs' );
-        $this->breadcrumbs = array();
-
-        if ( isset( $homeUrl ) && $this->pageTitle != $homeName ) {
-            $home[ 'url' ] = new System_Web_RawValue( $this->url( $homeUrl ) );
-            $home[ 'name' ] = $homeName;
-            $this->breadcrumbs[] = $home;
-        }
-
-        if ( !empty( $parents ) ) {
-            foreach ( $parents as $url => $name ) {
-                $parent[ 'url' ] = new System_Web_RawValue( $this->url( $url ) );
-                $parent[ 'name' ] = $name;
-                $this->breadcrumbs[] = $parent;
-            }
-        }
+        $principal = System_Api_Principal::getCurrent();
+        if ( $principal->isAuthenticated() )
+            $this->homeUrl = '/client/index.php';
+        else
+            $this->homeUrl = '/index.php';
 
         $scriptFiles = $this->view->getSlot( 'script_files' );
 
@@ -119,16 +72,9 @@ class Common_PageLayout extends System_Web_Component
 
         $this->cssConditional[ 'lt IE 8' ] = '/common/theme/ie7.css';
 
-        $inlineCode = $this->view->getSlot( 'inline_code', array() );
-
-        $session = System_Core_Application::getInstance()->getSession();
-        $path = $session->getCookiePath();
-        $secure = $session->isCookieSecure() ? 'true' : 'false';
-
-        array_unshift( $inlineCode, "
-            WebIssues.switchClient( 'mobile', { path: '$path', expires: 90, secure: $secure, raw: true } );" );
-
-        $this->inlineCode = new System_Web_RawValue( "    $( function() {" . join( '', $inlineCode ) . "\n    } );\n" );
+        $inlineCode = $this->view->getSlot( 'inline_code' );
+        if ( !empty( $inlineCode ) )
+            $this->inlineCode = new System_Web_RawValue( "    $( function() {" . join( '', $inlineCode ) . "\n    } );\n" );
 
         $this->icon = '/common/images/webissues.ico';
 
