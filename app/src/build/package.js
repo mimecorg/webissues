6ed13966921a62e41884e73599bed31186f4c2ba
mimecorg <mimec@mimec.org>
2018-05-18 20:27:51 +0200

    fix creating OS X client package on Windows

--- app/src/build/package.js
+++ app/src/build/package.js
@@ -17,15 +17,58 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 
-const archiver = require( 'archiver' );
 const fs = require( 'fs' );
-const packager = require( 'electron-packager' );
 const path = require( 'path' );
 
-const platform = process.argv[ 2 ];
-const arch = process.argv[ 3 ];
+const packager = require( 'electron-packager' );
+
+const Archiver = require( 'archiver/lib/core' );
+const zip = require( 'archiver/lib/plugins/zip' );
+
+const platform = process.argv[ 2 ] || process.platform;
+const arch = process.argv[ 3 ] || process.arch;
 
 const out = path.resolve( __dirname, '../../../packages' );
+
+if ( platform == 'darwin' && process.platform == 'win32' ) {
+  const _updateQueueTaskWithStats = Archiver.prototype._updateQueueTaskWithStats;
+
+  const executables = [
+    'crashpad_handler',
+    'Electron Framework',
+    'libffmpeg.dylib',
+    'libnode.dylib',
+    'Mantle',
+    'ReactiveCocoa',
+    'ShipIt',
+    'Squirrel',
+    'WebIssues',
+    'WebIssues Helper',
+    'WebIssues Helper EH',
+    'WebIssues Helper NP'
+  ];
+
+  Archiver.prototype._updateQueueTaskWithStats = function( task, stats ) {
+    task = _updateQueueTaskWithStats.call( this, task, stats );
+    if ( task.data.type == 'symlink' ) {
+      task.data.linkname = task.data.linkname.replace( /\\/g, '/' );
+      task.data.mode = 0755;
+    } else if ( task.data.type == 'file' ) {
+      if ( executables.includes( path.basename( task.data.name ) ) )
+        task.data.mode = 0755;
+      else
+        task.data.mode = 0644;
+    }
+    return task;
+  }
+}
+
+function createZipArchive( options ) {
+  const archive = new Archiver( 'zip', options );
+  archive.setFormat( 'zip' );
+  archive.setModule( new zip( options ) );
+  return archive;
+}
 
 packager( {
   name: 'WebIssues',
@@ -61,7 +104,7 @@ packager( {
   const zipname = 'webissues-client-' + platform + '-' + arch + '.zip';
 
   const output = fs.createWriteStream( path.join( out, zipname ) );
-  const archive = archiver( 'zip', { zlib: { level: 9 } } );
+  const archive = createZipArchive( { zlib: { level: 9 } } );
 
   archive.pipe( output );
   archive.directory( path.join( out, dirname ), false );
