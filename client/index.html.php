b14b353c231e96f9ec4c36525d4505cde2f227b4
mimecorg <mimec@mimec.org>
2020-01-27 17:46:15 +0100

    prevent automatic zooming in form fields on iOS

--- client/index.html.php
+++ client/index.html.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <title><?php echo $siteName ?></title>
   <link rel="shortcut icon" href="<?php echo $this->url( $icon ) ?>">
   <link rel="apple-touch-icon" href="<?php echo $this->url( $touchIcon ) ?>">
