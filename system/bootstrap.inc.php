1ffac5ff89fd77dece5c729c4b6b50aced99fcbb
mimecorg <mimec@mimec.org>
2021-10-05 17:54:43 +0200

    change version to 2.0.3

--- system/bootstrap.inc.php
+++ system/bootstrap.inc.php
@@ -33,7 +33,7 @@ if ( !function_exists( 'mb_strlen' ) )
 /**
 * Current version of WebIssues Server.
 */
-define( 'WI_VERSION', '2.0.2' );
+define( 'WI_VERSION', '2.0.3' );
 
 /**
 * Current version of the WebIssues database schema.
